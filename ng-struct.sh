3d538463c1f1f55ac1700274a5841bbeea85d9c1
Florian Beaumont <25250981+FlorianBx@users.noreply.github.com>
2025-01-25 12:40:23 +0100

    Update and rename ng-generate-struct.sh to ng-struct.sh

--- ng-struct.sh
+++ ng-struct.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Creation of the main structure
 mkdir -p src/app/{core/{guards,interceptors,interfaces,services},features/{navbar,header,footer},pages/{admin,home,login,"not-found"},shared/{components,directives,pipes}}
 
-echo "Jobs done ✅!"
+echo "Angular project structure created successfully!"
